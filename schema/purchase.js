7ffdab7fa4f191b4f73b8232de0370f12cfbce6c
crazy2engineers <49759084+crazy2engineers@users.noreply.github.com>
2019-08-15 18:40:02 +0530

    Update purchase.js

--- schema/purchase.js
+++ schema/purchase.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var purchase_schema = new Schema({
+supplier_name:String,
 supplierp_place:String,
 purchase_billno:String,
 date:Date,
@@ -11,11 +12,6 @@ purchase_unpaidamount:Number,
 pay_recno:String,
 pay_date:Date,
 dis : String,
-supplier:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:'supplier'
-}
-
 });
 
 module.exports = mongoose.model('purchase',purchase_schema);
